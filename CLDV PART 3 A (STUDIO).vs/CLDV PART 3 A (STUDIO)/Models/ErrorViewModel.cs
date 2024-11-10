923dbc9bc973f3550c89862139cb234dbd76b2c1
Michaelamm84 <ST10396724@vcconnect.edu.za>
2024-11-10 22:17:04 +0200

    creating project
    adding project to github

---
+++ CLDV PART 3 A (STUDIO).vs/CLDV PART 3 A (STUDIO)/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CLDV_PART_3_A__STUDIO_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
