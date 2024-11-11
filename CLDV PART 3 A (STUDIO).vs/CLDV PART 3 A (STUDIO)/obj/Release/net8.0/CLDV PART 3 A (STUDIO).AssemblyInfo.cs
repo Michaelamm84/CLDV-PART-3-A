4096ab16145dbf70f7a2f152ddfdf9fae2d5068b
Michaelamm84 <ST10396724@vcconnect.edu.za>
2024-11-11 17:30:53 +0200

    updated project
    update

--- CLDV PART 3 A (STUDIO).vs/CLDV PART 3 A (STUDIO)/obj/Release/net8.0/CLDV PART 3 A (STUDIO).AssemblyInfo.cs
+++ CLDV PART 3 A (STUDIO).vs/CLDV PART 3 A (STUDIO)/obj/Release/net8.0/CLDV PART 3 A (STUDIO).AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("CLDV PART 3 A (STUDIO)")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+923dbc9bc973f3550c89862139cb234dbd76b2c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CLDV PART 3 A (STUDIO)")]
